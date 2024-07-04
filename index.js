78cdee6ed5eb523f7e95b6e6395a8f09fea6afe1
unknown <dalton.joyner1@gmail.com>
2024-07-03 18:18:49 -0600

    Calculator

---
+++ index.js
@@ -0,0 +1,22 @@
+// calc functions
+
+const display = document.getElementById('display');
+
+function moveToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
+
+
